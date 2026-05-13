3e135e46ee911f8230a1c0a311b1a3a625b7bff7
user.name <adeolagodsonomomowo@gmail.com>
2026-05-13 23:57:08 +0100

    feat: added a feature to add meal using useRef

--- src/AddFood.jsx
+++ src/AddFood.jsx
@@ -1,30 +1,35 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useRef } from "react";
 import { ItemPortal } from "./context";
+
 
 export default function AddFood() {
   const [newFood, setNewFood] = useState("");
   const { items, setItems } = useContext(ItemPortal);
 
+  const inputRef = useRef(null);
+
   const addFood = (e) => {
+    
+    const food = inputRef.current.value;
+    setNewFood(food);
     setItems([...items, newFood]);
-    setNewFood("");
+   
   };
 
-  const changehandler =(e)=>{
-     setNewFood(e.target.value);
-     
-  }
+    // const changehandler =(e)=>{
+    //    setNewFood(e.target.value);
+
+    // }
 
   return (
     <div className="flex">
       <textarea
+        ref={inputRef}
         rows={5}
         column={100}
-        value={newFood}
         placeholder="Enter food and calories here"
-        onChange={changehandler}
       ></textarea>
-      
+
       <button onClick={addFood}>Add Food</button>
     </div>
   );
